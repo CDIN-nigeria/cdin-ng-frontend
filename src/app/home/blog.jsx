5013426ccbbabd1350fdecae5c0e98a0a06e2094
Jerydam <jerydan148@gmail.com>
2023-10-20 08:06:25 +0100

    pending......

--- src/app/home/blog.jsx
+++ src/app/home/blog.jsx
@@ -1,35 +1,9 @@
-import React from 'react';
+import React from 'react'
 
-const Blog = ({ posts }) => {
-    return (
-        <div>
-            {posts.map((post, index) => (
-                <div key={index}>
-                    <h2>{post.title}</h2>
-                    <p>{post.content}</p>
-                    <hr />
-                </div>
-            ))}
-        </div>
-    );
-};
-
-const Index = ({ posts }) => {
-    return (
-        <div>
-            <h1>Welcome to My Landing Page</h1>
-            <Blog posts={posts} />
-        </div>
-    );
-};
-
-export default Index;
-
-export async function getStaticProps() {
-    // Fetch data from an API
-    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const posts = await res.json();
-
-    // Pass data to the page via props
-    return { props: { posts } };
+const Blog = () => {
+  return (
+    <div> Blog</div>
+  )
 }
+
+export default Blog
